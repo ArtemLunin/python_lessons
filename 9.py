3f91de652ba824c99519fb4517d465abbdf518fc
ArtemLunin <lunin.mail@gmail.com>
2021-04-28 20:15:18 +0300

    cahper 9

--- 9.py
+++ 9.py
@@ -1,5 +1,5 @@
 # в функцию можно передавать именованные аругменты
-menu(entree='beef', dessert='bagel', wine='bordeaux')
+# menu(entree='beef', dessert='bagel', wine='bordeaux')
 
 # аргументы по умолчанию
 # def menu(wine, entree, dessert='pudding')
@@ -15,3 +15,40 @@ menu(entree='beef', dessert='bagel', wine='bordeaux')
 
 #  аргументы после * - передаются как именованные
 # def print_data(data, *, start=0, end=100)
+
+def get_odds():
+	for number in range(10):
+		if number % 2 == 1:
+			yield number
+
+count = 0
+for i in get_odds():
+	count += 1
+	if count == 3:
+		print(i)
+		break
+
+
+
+def test(func):
+	def new_func(*args, **kwargs):
+		print('start')
+		print(func(*args, **kwargs))
+		print('end')
+	return new_func
+
+@test
+def simple():
+	return 'SIMPLE'
+
+simple()
+
+
+class OopsException(Exception):
+	pass
+
+
+try:
+	raise OopsException()
+except OopsException as exc:
+	print('Caught an oops')
